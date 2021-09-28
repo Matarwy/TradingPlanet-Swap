e3d4818bb8924f1b324cb76f862187c274b8a850
elhassan elmatarwy <elhassan214@gmail.com>
2021-09-28 23:11:26 +0200

    Update bep20.js

--- src/front/config/mainnet/bep20.js
+++ src/front/config/mainnet/bep20.js
@@ -3,7 +3,7 @@ export default {
     address: '0x92648e4537cdfa1ee743a244465a31aa034b1ce8',
     decimals: 18,
     fullName: 'SWAP',
-    canSwap: true,
+    canSwap: false,
   },
   btcb: {
     address: '0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c',
