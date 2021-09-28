eedc58be84f4623047c86a1833c15b90366d183d
elhassan elmatarwy <elhassan214@gmail.com>
2021-09-28 18:01:25 +0200

    Update erc20.js

--- src/front/config/mainnet/erc20.js
+++ src/front/config/mainnet/erc20.js
@@ -3,7 +3,7 @@ export default {
     address: '0x14a52cf6B4F68431bd5D9524E4fcD6F41ce4ADe9',
     decimals: 18,
     fullName: 'SWAP',
-    canSwap: true,
+    canSwap: false,
   },
   pay: {
     address: '0x1fe72034da777ef22533eaa6dd7cbe1d80be50fa',
