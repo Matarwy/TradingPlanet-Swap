e330d2509fa81752a56f42d2fe7ed277cea152c4
elhassan elmatarwy <elhassan214@gmail.com>
2021-09-29 13:13:11 +0200

    Update erc20.js

--- src/front/config/mainnet/erc20.js
+++ src/front/config/mainnet/erc20.js
@@ -5,30 +5,30 @@ export default {
   //   fullName: 'SWAP',
   //   canSwap: false,
   // },
-  // pay: {
-  //   address: '0x1fe72034da777ef22533eaa6dd7cbe1d80be50fa',
-  //   decimals: 18,
-  //   fullName: 'PayAccept',
-  //   canSwap: false,
-  // },
-  // snm: {
-  //   address: '0x983f6d60db79ea8ca4eb9968c6aff8cfa04b3c63',
-  //   decimals: 18,
-  //   fullName: 'snm',
-  //   canSwap: false,
-  // },
+  pay: {
+    address: '0x1fe72034da777ef22533eaa6dd7cbe1d80be50fa',
+    decimals: 18,
+    fullName: 'PayAccept',
+    canSwap: false,
+  },
+  snm: {
+    address: '0x983f6d60db79ea8ca4eb9968c6aff8cfa04b3c63',
+    decimals: 18,
+    fullName: 'snm',
+    canSwap: false,
+  },
   eurs: {
     address: '0xdb25f211ab05b1c97d595516f45794528a807ad8',
     decimals: 2,
     fullName: 'EURS',
     canSwap: true,
   },
-  // xeur: {
-  //   address: '0xC31C61cf55fB5E646684AD8E8517793ec9A22c5e',
-  //   decimals: 0,
-  //   fullName: 'XEUR',
-  //   canSwap: false,
-  // },
+  xeur: {
+    address: '0xC31C61cf55fB5E646684AD8E8517793ec9A22c5e',
+    decimals: 0,
+    fullName: 'XEUR',
+    canSwap: false,
+  },
   usdt: {
     address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
     decimals: 6,
